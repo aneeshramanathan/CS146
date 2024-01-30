34832519941b1c4ecb07c42fa92c15e8c8f9d140
Aneesh <aneesh.ramanathan@gmail.com>
2024-01-29 19:18:32 -0800

    Third commit

--- Labs/Lab1/pythonpart.py
+++ Labs/Lab1/pythonpart.py
@@ -1,14 +1,13 @@
 class pythonpart:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
-        for i in range (n-1):
+        for i in range (0,n-1):
             for j in range (i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return []
     
-    def main():
-        array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        arrayTwo = twoSum(array, 17)
-        for i in arrayTwo:
-            print(i)
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    arrayTwo = twoSum(array, 17)
+    for i in arrayTwo:
+        print(i)
