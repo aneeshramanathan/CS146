b769f059afe1b01f4e4adadadd73c45251ffe0f3
aneeshramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-01-31 15:18:38 -0800

    Update pythonpart.py

--- Labs/Lab2/pythonpart.py
+++ Labs/Lab2/pythonpart.py
@@ -1,1 +1,16 @@
+import string
+
+
+class pythonpart:
+    def validAnagram(s: string, t: string) -> bool:
+        if len(s) != len(t):
+            return False
+        array = sorted(s)
+        arrayTwo = sorted(t)
+        for i in range(len(s)):
+            if array[i] != arrayTwo[i]:
+                return False
+        return True
+    
+    print(validAnagram("tea", "eat"))
 
