2f7e47c2b6050243de3bd7a6b6de1d0d4238d3bf
aneeshramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-01-31 15:17:42 -0800

    Update javapart.java

--- Labs/Lab2/javapart.java
+++ Labs/Lab2/javapart.java
@@ -1,1 +1,28 @@
+package Labs.Lab2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class javapart {
+    public static boolean validAnagram(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        char [] array = s.toCharArray();
+        char [] arrayTwo = t.toCharArray();
+        Arrays.sort(array);
+        Arrays.sort(arrayTwo);
+        for(int i=0; i < s.length(); i++){
+            if(array[i] != arrayTwo[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    public static void main (String [] args){
+        System.out.println(validAnagram("tea", "eat"));
+    }
+}
 
