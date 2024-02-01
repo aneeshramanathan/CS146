2eeb529678c0d12675bc10b9ec2a1a458c81cbac
aneeshramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-02-01 14:44:20 -0800

    Update pythonpart.py

--- Homework/Homework2/pythonpart.py
+++ Homework/Homework2/pythonpart.py
@@ -1,1 +1,15 @@
 
+class pythonpart:
+    def firstBad(n):
+        least = 1
+        greatest = n
+        while least < greatest:
+            mid = least + (greatest - least) // 2
+            if isBadVersion(mid):
+                greatest = mid
+            else:
+                least = mid + 1
+        return least
+    
+    print(firstBad(4))
+    
