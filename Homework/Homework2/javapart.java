eef12dcb2d4fb1c910dc9d57553c4c1244003dce
aneeshramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-02-01 10:05:50 -0800

    Update javapart.java

--- Homework/Homework2/javapart.java
+++ Homework/Homework2/javapart.java
@@ -1,1 +1,19 @@
+package Homework.Homework2;
 
+
+public class javapart extends VersionControl {
+    public int firstBad(int n) {
+        int least = 1;
+        int greatest = n;
+        while(least < greatest){
+            int mid = least + (greatest - least) / 2;
+            if(isBadVersion(mid)){
+                greatest = mid;
+            }
+            else{
+                least = mid + 1;
+            }
+        }
+        return least;
+    }
+}
