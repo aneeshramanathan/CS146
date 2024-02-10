03428b0de1f926b3b32dfc62d8022c22dbe0c6a3
aneesh <aneesh.ramanathan@gmail.com>
2024-02-09 16:53:16 -0800

    first commit

--- BonusQuestion/BonusQuestion1/pythonpart.py
+++ BonusQuestion/BonusQuestion1/pythonpart.py
@@ -1,1 +1,12 @@
-
+class pythonpart:
+    def nFibonacci(n: int) -> int:
+        first = 0
+        second = 1
+        third = None
+        for i in range (2, n+1):
+            third = first + second
+            first = second
+            second = third
+        return second
+    
+    print(nFibonacci(9))
