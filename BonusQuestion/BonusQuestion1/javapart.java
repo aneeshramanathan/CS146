1da0103dc7e394b3d4ba9d69911070cc9c40a435
aneesh <aneesh.ramanathan@gmail.com>
2024-02-09 16:43:58 -0800

    First change

--- BonusQuestion/BonusQuestion1/javapart.java
+++ BonusQuestion/BonusQuestion1/javapart.java
@@ -1,1 +1,18 @@
+package BonusQuestion.BonusQuestion1;
 
+class javapart{
+    public static int nFibonacci(int n){
+        int first = 0;
+        int second = 1;
+        int third; 
+        for(int i = 2; i <= n; i++ ){  
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return second; 
+    }
+    public static void main(String[] args) {
+        System.out.println(nFibonacci(9));
+    }
+}
