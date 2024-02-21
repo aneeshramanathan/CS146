55a7c4a23287a7dfff6402b58eadfa3507560dc0
Aneesh <aneesh.ramanathan@gmail.com>
2024-02-21 10:45:59 -0800

    first commit

--- Homework/Homework7/pythonpart.py
+++ Homework/Homework7/pythonpart.py
@@ -1,1 +1,18 @@
+import heapq
+from typing import Self
+
+
+def minMeetingRooms(self, intervals: list[list[int]]) -> int:   
+    intervals.sort(key=lambda x: x[0])
+    serverEndTimes=[]
+    for start, end in intervals:
+        while serverEndTimes and serverEndTimes[0] <= start:
+            heapq.heappop(serverEndTimes)
+        heapq.heappush(serverEndTimes, end)
+    return len(serverEndTimes)
+
+intervals = [[0,30],[5,10],[15,20]]
+print(minMeetingRooms(Self, intervals))
+intervalsTwo = [[0,1],[1,2],[2,3]]
+print(minMeetingRooms(Self, intervalsTwo))
 
