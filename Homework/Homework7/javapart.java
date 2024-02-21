350776cbf6cd96d431f92501fa88242dfcbb575b
Aneesh <aneesh.ramanathan@gmail.com>
2024-02-20 21:03:12 -0800

    second commit

--- Homework/Homework7/javapart.java
+++ Homework/Homework7/javapart.java
@@ -4,22 +4,28 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class javapart{
-    public int minMeetingRooms(int[][] intervals) {  
+    public static int minMeetingRooms(int[][] intervals) {  
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         PriorityQueue<Integer> endTimes = new PriorityQueue<Integer>();
-        int min = 0;
         for(int i=0; i<intervals.length; i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
-            int index = -1;
-            for(int j=0; j<min; j++){
-                if(){
-                    
-                }
+            while(!endTimes.isEmpty() && endTimes.peek()<=start){
+                endTimes.poll();
             }
+            endTimes.offer(end);
         }
+        return endTimes.size();
+    } 
 
-        return min;
+
+    public static void main(String[] args){
+        int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
+        System.out.println(minMeetingRooms(intervals)); 
+        int[][] intervalsTwo = {{0, 1}, {1, 2}, {2, 3}};
+        System.out.println(minMeetingRooms(intervalsTwo));
+        int[][] intervalsThree = {{1, 3}, {2, 5}, {3, 6}};
+        System.out.println(minMeetingRooms(intervalsThree));
     }
 }
 
