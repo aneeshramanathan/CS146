c00212c1b9736cea2839393e3cd6bc9005a0e314
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-02-26 14:55:33 -0800

    Update pythonpart.py

--- Homework/Homework8/pythonpart.py
+++ Homework/Homework8/pythonpart.py
@@ -1,19 +1,18 @@
 def longestPalindrome(self, s):
-  if s is None:
-    return 0
-  map = {}
-  for i in range(len(s)):
-    x = s[i]
-    if x in map:
-        map[x] += 1
-    else:
-        map[x] = 1
-  count = 0
-  for key in map:
-    if list(map.keys()).index(key) % 2 == 0:
-        count += 1
-  if count == 0:
-    return len(s)
-  else:
-    return len(s) - count + 1
-
+        if s is None:
+            return 0
+        map = {}
+        for i in range(len(s)):
+            x = s[i]
+            if x in map:
+                map[x] += 1
+            else:
+                map[x] = 1
+        count = 0
+        for key in map:
+            if list(map.keys()).index(key) % 2 == 0:
+                count += 1
+        if count == 0:
+            return len(s)
+        else:
+            return len(s) - count + 1
