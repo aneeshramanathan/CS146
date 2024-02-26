c1a1da62545602965ea195303ae474c86b957393
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-02-26 15:23:54 -0800

    Update javapart.java

--- Homework/Homework8/javapart.java
+++ Homework/Homework8/javapart.java
@@ -1,27 +1,22 @@
 public int longestPalindrome(String s) {
-  if(s == null){
+       if (s == null || s.isEmpty()) {
             return 0;
         }
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            char x = s.charAt(i);
-            if(map.containsKey(x)){
-                map.put(x, map.get(x)+1);
-            }
-            else{
-                map.put(x, 1);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int oddCount = 0;
+        for (char c : map.keySet()) {
+            int occurences = map.get(c);
+            if (occurences % 2 != 0) {
+                oddCount++;
             }
         }
-        int count = 0;
-        for(int i = 0; i < map.size(); i++){
-            if(i%2 == 0){
-                count++;
-            }
-        }
-        if(count == 0){
+        if (oddCount == 0) {
             return s.length();
-        }
-        else{
-            return s.length() - count + 1;
+        } else {
+            return s.length() - oddCount + 1;
         }
     }
