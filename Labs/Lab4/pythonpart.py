b2eade6fd947f9ac1631237101e22ef661ee4f79
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-06 14:39:29 -0800

    Update pythonpart.py

--- Labs/Lab4/pythonpart.py
+++ Labs/Lab4/pythonpart.py
@@ -1,1 +1,9 @@
-
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        x = root.left
+        root.left = root.right
+        root.right = x
+        return root
