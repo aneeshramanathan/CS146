da88b5840ae8ae0044e11cf1e7be1133dffeec91
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-08 14:01:10 -0800

    Update pythonpart.py

--- Labs/Lab5/pythonpart.py
+++ Labs/Lab5/pythonpart.py
@@ -1,1 +1,21 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+    def isValidBST(self, root):
+        if root is None:
+            return True
+        if root.left is not None and root.left.val > root.right.val:
+            return False
+        if root.right is not None and root.left.val < root.right.val:
+            return False
+        left = self.isValidBST(root.left)
+        right = self.isValidBST(root.left)
+        if left == True and right == True:
+            return True
+        else:
+            return False
+
 
