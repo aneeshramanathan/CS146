9c679b7d2d6a29d768dc2e235963cb063759eade
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-11 21:39:30 -0700

    Update pythonpart.py

--- Homework/Homework10/pythonpart.py
+++ Homework/Homework10/pythonpart.py
@@ -1,1 +1,18 @@
-
+def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        solution = []
+        if root is None:
+            return solution
+        levels = []
+        levels.append(root)
+        while levels:
+            levelSize = len(levels)
+            currentLevel = []
+            for i in range(levelSize):
+                node = levels.pop(0)
+                currentLevel.append(node.val)
+                if node.left:
+                    levels.append(node.left)
+                if node.right:
+                    levels.append(node.right)
+            solution.append(currentLevel)
+        return solution
