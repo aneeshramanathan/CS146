f34de5679a2880208c74646d016fc8093d099a4e
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-13 14:18:27 -0700

    Update pythonpart.py

--- Labs/Lab5/pythonpart.py
+++ Labs/Lab5/pythonpart.py
@@ -4,26 +4,16 @@ class TreeNode(object):
          self.left = left
          self.right = right
     def isValidBST(self, root):
-        if root is None:
-            return True
-        leftMax = self.max_val(root.left)
-        rightMin = self.min_val(root.right)
-        if leftMax > root.val or rightMin < root.val:
-            return False
-        left = self.isValidBST(root.left)
-        right = self.isValidBST(root.right)
-        if left and right:
-            return True
-        else:
-            return False
-    def max_val(self, root):
-        while root.right is not None:
-            root = root.right
-        return root.val
-    def min_val(self, root):
-        while root.left is not None:
-            root = root.left
-        return root.val
+        prev = float('-inf')
+        def inorder(node):
+            nonlocal prev
+            if not node:
+                return True
+            if not (inorder(node.left) and prev < node.val):
+                return False
+            prev = node.val
+            return inorder(node.right)
+        return inorder(root)
 
 
 
