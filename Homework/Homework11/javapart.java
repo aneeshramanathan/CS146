668186ac65658f54b199c5990bb81961e9e94e59
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-16 15:10:51 -0700

    Update javapart.java

--- Homework/Homework11/javapart.java
+++ Homework/Homework11/javapart.java
@@ -1,1 +1,17 @@
-
+public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] == color){
+            return image;
+        }
+        depthFirstSearch(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+    public void depthFirstSearch(int image[][], int sr, int sc, int originalColor, int newColor){
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != originalColor) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        depthFirstSearch(image, sr-1, sc, originalColor, newColor);
+        depthFirstSearch(image, sr+1, sc, originalColor, newColor);
+        depthFirstSearch(image, sr, sc-1, originalColor, newColor);
+        depthFirstSearch(image, sr, sc+1, originalColor, newColor);
+    }
