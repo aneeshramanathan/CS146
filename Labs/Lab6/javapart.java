b9dd85e95007ffa566631fc68bc3ad827c34649f
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-20 14:47:47 -0700

    Update javapart.java

--- Labs/Lab6/javapart.java
+++ Labs/Lab6/javapart.java
@@ -1,1 +1,28 @@
-
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+        for (int[] prerequisite : prerequisites) {
+            list.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int neighbor : list.get(course)) {
+                if (--indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return count == numCourses;
+    }
