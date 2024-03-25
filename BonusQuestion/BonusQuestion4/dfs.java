dcf882413896d7a6d375cf95ce544ab738f87e93
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-25 15:11:06 -0700

    Update dfs.java

--- BonusQuestion/BonusQuestion4/dfs.java
+++ BonusQuestion/BonusQuestion4/dfs.java
@@ -1,1 +1,34 @@
-
+public static List<Integer> topologicalSort(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prerequisite = edge[1];
+            graph.get(course).add(prerequisite); // Reversed edge direction for DFS
+        }
+        boolean[] visited = new boolean[numCourses];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited, stack);
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        
+        return result;
+    }
+    
+    private static void dfs(int node, List<List<Integer>> graph, boolean[] visited, Stack<Integer> stack) {
+        visited[node] = true;
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, graph, visited, stack);
+            }
+        }
+        stack.push(node); // Push the node to stack after processing its neighbors
+    }
