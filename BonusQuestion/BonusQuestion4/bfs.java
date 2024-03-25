589af04d28cb4fb03e83f654041546a1ecbcdd4b
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-03-25 15:00:30 -0700

    Update bfs.java

--- BonusQuestion/BonusQuestion4/bfs.java
+++ BonusQuestion/BonusQuestion4/bfs.java
@@ -1,1 +1,35 @@
-
+public static List<Integer> topologicalSort(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] inDegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+      int course = prerequisites[i][0];
+      int prerequisite = prerequisites[i][1];
+      graph.get(prerequisite).add(course);
+      inDegree[course]++;
+      }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            result.add(current);
+            for (int neighbor : graph.get(current)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        if (result.size() != numCourses) {
+            return new ArrayList<>();
+        } else {
+            return result;
+        }
+    }
