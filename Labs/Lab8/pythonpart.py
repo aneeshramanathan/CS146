df289fdbddef54bb2053e56bf4a7cd5ece21a9b1
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-04-22 15:29:38 -0700

    Update pythonpart.py

--- Labs/Lab8/pythonpart.py
+++ Labs/Lab8/pythonpart.py
@@ -1,14 +1,9 @@
 def coinChange(self, coins, amount):
         dp = [float('inf')] * (amount + 1)
-        dp[0] = 0  # Base case: 0 coins needed to make amount 0
-
-        # Iterate through each coin denomination
+        dp[0] = 0
         for coin in coins:
-            # For each amount from coin to amount, update the minimum number of coins needed
             for i in range(coin, amount + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-
-        # Check if dp[amount] is still float('inf')
         if dp[amount] != float('inf'):
             return dp[amount]
         else:
