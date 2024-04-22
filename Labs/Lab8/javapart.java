6ec89632aa4f86831949c72af055134d26e71e4b
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-04-22 15:27:18 -0700

    Update javapart.java

--- Labs/Lab8/javapart.java
+++ Labs/Lab8/javapart.java
@@ -1,1 +1,19 @@
-
+public int coinChange(int[] coins, int amount) {
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0; 
+    for (int i = 0; i < coins.length; i++) {
+        int coin = coins[i];
+        for (int j = coin; j <= amount; j++) {
+            if (dp[j - coin] != Integer.MAX_VALUE) {
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+            }
+        }
+    }
+    if (dp[amount] != Integer.MAX_VALUE) {
+        return dp[amount];
+    } 
+    else{
+        return -1;
+    }
+}
